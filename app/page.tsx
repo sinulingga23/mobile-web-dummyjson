e96295b392471f1cbfa2a2c11bf9be95a7e7b972
Denny Rezky Sinulingga <sinulinggatwo@gmail.com>
2024-05-14 10:32:55 +0700

    add bottom navigation

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,15 @@
+import BottomNavigation from "../component/BottomNavigation";
 
 export default function Home() {
   return (
-    <p>WIP!</p>
+    <section>
+      <div className="fixed left-0 z-50 w-full h-16 bg-white border-t border-gray-200">
+        <div className="grid h-full max-w-lg grid-cols-4 mx-auto font-medium">
+          Content
+        </div>
+      </div>
+
+      <BottomNavigation />
+    </section>
   );
 }
